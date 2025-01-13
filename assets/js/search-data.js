267f14d7334330302bfcc2b4c56112093c55cedd
zbpasek <zbigniew_pasek@o2.pl>
2025-01-13 18:38:05 +0000

    Deploying to gh-pages from @ zbpasek/zbpasek.github.io@ae4c8be7f2f7ff63a5a4478398593dd6f3fa332d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -368,62 +368,41 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+    },{id: "grants-etos-górniczy-w-20-lat-po-zamknięciu-kopalń",
+          title: 'Etos górniczy w 20 lat po zamknięciu kopalń',
+          description: "Grant rektora AGH dla Koła Naukowego Kulturoznawców (opieka merytoryczna i realizacja badań terenowych w Wałbrzychu)",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/etos-gorniczy-w-20-lat-po-zamknieciu-kopaln/";
+            },},{id: "grants-kulturowe-aspekty-strategii-promocyjnych-wałbrzycha",
+          title: 'Kulturowe aspekty strategii promocyjnych Wałbrzycha',
+          description: "Grant rektora AGH dla Koła Naukowego Kulturoznawców (opieka merytoryczna i realizacja badań terenowych)",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/kulturowe-aspekty-strategii-promocyjnych-walbrzycha/";
+            },},{id: "grants-kulturowe-determinanty-postaw-proekologicznych-na-przykładzie-wisły",
+          title: 'Kulturowe determinanty postaw proekologicznych na przykładzie Wisły',
+          description: "Grant rektora AGH dla Kulturoznawczego Koła Naukowego (opieka merytoryczna i realizacja badań terenowych)",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/kulturowe-determinanty-postaw-proekologicznych-na-przykladzie-wisly/";
+            },},{id: "grants-mistycyzm-pentekostalny-w-polsce",
+          title: 'Mistycyzm pentekostalny w Polsce',
+          description: "Grant promotorski Narodowego Centrum Nauki (dr Andrzej Migda)",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/mistycyzm-pentekostalny-w-polsce/";
+            },},{id: "grants-skarb-jako-szansa-na-wyjście-z-szybu",
+          title: 'Skarb jako szansa na wyjście z szybu',
+          description: "Grant rektora AGH dla Koła Naukowego Kulturoznawców (opieka merytoryczna i realizacja badań terenowych w Wałbrzychu)",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/skarb-jako-szansa-na-wyjscie-z-szybu/";
+            },},{id: "grants-wspólnoty-religijne-wisły",
+          title: 'Wspólnoty religijne Wisły',
+          description: "Grant badawczy i wydawniczy Rady Miasta Wisły",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/wspolnoty-religijne-wisly/";
+            },},{id: "grants-zabytki-w-krajobrazie-kulturowym-powiatu-wałbrzyskiego",
+          title: 'Zabytki w krajobrazie kulturowym powiatu wałbrzyskiego',
+          description: "Grant rektora AGH dla Kulturoznawczego Koła Naukowego (opieka merytoryczna i realizacja badań terenowych)",
+          section: "Grants",handler: () => {
+              window.location.href = "/grants/zabytki-w-krajobrazie-kulturowym-powiatu-walbrzyskiego/";
             },},{
         id: 'social-email',
         title: 'email',
